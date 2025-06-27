c61e87689aa3708f8037848694da7de978194cb1
Adam-445 <Adamch012016@outlook.com>
2025-06-27 14:07:40 +0100

    feat(processing): improve device categorizer

--- services/processing/src/transformations/device_categorizer.py
+++ services/processing/src/transformations/device_categorizer.py
@@ -3,9 +3,10 @@ from pyflink.table import expressions as expr
 
 def categorize_device(table):
     return table.add_columns(
-        expr.col("device")
-        .get("user_agent")
-        .like("%Mobile%")
-        .then("Mobile", "Desktop")
+        expr.case()
+        .when(expr.col("device").get("user_agent").like("%Mobile%"), "Mobile")
+        .when(expr.col("device").get("user_agent").like("%Tablet%"), "Tablet")
+        .when(expr.col("device").get("user_agent").like("%Bot%"), "Bot")
+        .else_value("Desktop")
         .alias("device_category")
     )
