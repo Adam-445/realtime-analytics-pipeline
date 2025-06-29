171a12a51ae8e9907f9bdc3821b02aa0ee48f348
Adam-445 <Adamch012016@outlook.com>
2025-06-29 15:47:57 +0100

    fix(processing): Replace unsupported expr.case() with nested conditional expressions in device categorization

--- services/processing/src/transformations/device_categorizer.py
+++ services/processing/src/transformations/device_categorizer.py
@@ -2,11 +2,12 @@ from pyflink.table import expressions as expr
 
 
 def categorize_device(table):
-    return table.add_columns(
-        expr.case()
-        .when(expr.col("device").get("user_agent").like("%Mobile%"), "Mobile")
-        .when(expr.col("device").get("user_agent").like("%Tablet%"), "Tablet")
-        .when(expr.col("device").get("user_agent").like("%Bot%"), "Bot")
-        .else_value("Desktop")
-        .alias("device_category")
-    )
+    ua = expr.col("device").get("user_agent")
+    category = (
+        ua.like("%Mobile%").then(
+            "Mobile",
+            ua.like("%Tablet%").then("Tablet", ua.like("%Bot%").then("Bot", "Desktop")),
+        )
+    ).alias("device_category")
+
+    return table.add_columns(category)
